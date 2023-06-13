a12a72ebcdd4039738856ea997fd7e94a9d508a5
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-13 10:56:23 +0300

    comlpleted arithmetic labs

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31, num2 =2, num3 =39, num4 =5;
+
+let multiply;
+multiply = num1 * num2;
+
+let random;
+random = Math.round(Math.random()*10)
+
+let mod;
+mod = Number.parseInt(num3) % Number.parseInt(num4);
+
+let max;
+max = Math.max(1,5,8,20)
